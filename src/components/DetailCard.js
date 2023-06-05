ae1bef4b71217cb2e0293eef18cb111ec42098bf
4bhish <108976136+4bhish@users.noreply.github.com>
2023-06-05 17:53:45 +0530

    Update DetailCard.js

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -1,32 +1,39 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
 
+
 import '../styles/DetailCard.css'
 import { LoginProvider } from '../contexts/LoginContext';
 
-function DetailCard({product,handleAddToCart,cart}) {
+function DetailCard({ product, handleAddToCart, cart }) {
+  const discountedPrice = (product.price - (product.price * product.discountPercentage) / 100).toFixed(2);
+  const { loginStatus } = useContext(LoginProvider);
 
-    const discountedPrice = (product.price - (product.price * product.discountPercentage) / 100).toFixed(2);
-    const {loginStatus} = useContext(LoginProvider)
+  function alertFunc() {
+    return alert("You are not Logged-In");
+  }
 
-    function alertFunc(){
-      return alert("You are not Logged-In")
-    }
   return (
     <div className="product__detail__content">
-        <img src={product.thumbnail} alt={product.title} className="product__detail__image" />
-        <div className="product__detail__info">
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
-          <p>Price: ${discountedPrice}</p>
-          <p>Rating: {product.rating}</p>
-          <p>Category: {product.category}</p>
-          {
-            cart.includes(product) ? <Link style={{backgroundColor:'green',textAlign:'center'}} className="action__button" to="/cart">Go to Cart</Link> : <button className="action__button" onClick={loginStatus ? () => handleAddToCart(product) : alertFunc}>Add to Cart</button>
-          }
-        </div>
+      <img src={product.thumbnail} alt={product.title} className="product__detail__image" />
+      <div className="product__detail__info">
+        <h2>{product.title}</h2>
+        <p>{product.description}</p>
+        <p>Price: ${discountedPrice}</p>
+        <p>Rating: {product.rating}</p>
+        <p>Category: {product.category}</p>
+        {cart.includes(product) ? (
+          <Link style={{ backgroundColor: 'green', textAlign: 'center' }} className="action__button" to="/cart">
+            Go to Cart
+          </Link>
+        ) : (
+          <button className="action__button" onClick={loginStatus ? () => handleAddToCart(product) : alertFunc}>
+            Add to Cart
+          </button>
+        )}
       </div>
-  )
+    </div>
+  );
 }
 
-export default DetailCard
+export default DetailCard;
